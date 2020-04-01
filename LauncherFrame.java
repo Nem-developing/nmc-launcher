9b1bd1dfeae376164608d2f785b8251c2b255d82
Nem-developing <54665744+Nem-developing@users.noreply.github.com>
2020-04-01 10:16:50 +0200

    Create LauncherFrame.java

---
+++ LauncherFrame.java
@@ -0,0 +1,46 @@
+package fr.nemixcraft.launcher;
+
+
+import javax.swing.JFrame;
+
+import fr.theshark34.swinger.Swinger;
+import fr.theshark34.swinger.util.WindowMover;
+
+@SuppressWarnings("serial")
+public class LauncherFrame extends JFrame{
+
+	private static LauncherFrame instance;
+	private LauncherPanel launcherPanel;
+	
+	public LauncherFrame() {
+		this.setTitle("Nemixcraft Launcher");
+		this.setSize(874, 561);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.setUndecorated(true);
+		this.setIconImage(Swinger.getResource("icon.png"));
+		this.setContentPane(launcherPanel = new LauncherPanel ());
+		
+		WindowMover mover = new WindowMover(this); 
+		this.addMouseListener(mover);
+		this.addMouseMotionListener(mover);
+		
+		
+		this.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		Swinger.setSystemLookNFeel();
+		Swinger.setResourcePath("/fr/nemixcraft/launcher/resources/");
+		
+		instance = new LauncherFrame();
+	}
+	
+	public static LauncherFrame getInstance() {
+		return instance;
+	}
+
+	public LauncherPanel getLauncherPanel() {
+		return this.launcherPanel;
+	}
+}
